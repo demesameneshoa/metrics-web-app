980d3594b6a117f0605b884b2c1c75f7aa0190cc
demesameneshoa <demesameneshoa@gmail.com>
2023-08-03 16:37:37 +0300

    Fix linters errors

--- src/tests/Details.test.js
+++ src/tests/Details.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from '../redux/store';
 import '@testing-library/jest-dom/extend-expect';
 import Details from '../pages/Details';
-import { MemoryRouter } from 'react-router-dom';
 
 describe('Cities page Renders Properly ', () => {
   const component = (
@@ -19,7 +19,7 @@ describe('Cities page Renders Properly ', () => {
     const tree = render(component);
     expect(tree).toMatchSnapshot();
   });
-  
+
   test('Should render the city lnks', () => {
     render(component);
     const element = screen.getByRole('link');
